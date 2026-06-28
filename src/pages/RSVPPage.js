0b6febc482657da753cdcc743faa3521ad21b083
Dominic <dominic.clark@gmail.com>
2026-06-27 18:13:23 -0700

    Remove unused useCallback import

--- src/pages/RSVPPage.js
+++ src/pages/RSVPPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef } from 'react';
 import BotanicalDivider from '../components/BotanicalDivider';
 import styles from './RSVPPage.module.css';
 
